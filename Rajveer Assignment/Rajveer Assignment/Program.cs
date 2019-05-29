d459460c54f08ec4572284c0b834ceecb4048ad6
Rajveer Kaur <30013158@student.toiohomai.ac.nz>
2019-05-30 10:18:22 +1200

    first

--- Rajveer Assignment/Rajveer Assignment/Program.cs
+++ Rajveer Assignment/Rajveer Assignment/Program.cs
@@ -13,50 +13,65 @@ namespace Rajveer_Assignment
         static void Main(string[] args)
         {
 
-            Console.WriteLine("**************Welcome to our new Login system**************");
+            Console.WriteLine("**************  Welcome to our new Login system  **************");
+
+            //initialising result outside of loops
             string result = "";
+
 
             do
             {
                 Console.Write("Please enter your username: ");
                 string userName = Console.ReadLine();
-                Console.WriteLine(ValidateUsername(userName));//Calling the ValidateUsername method
+                Console.WriteLine(ValidateUsername(userName));
+                result = ValidateUsername(userName);//Calling the ValidateUsername method
                 Console.WriteLine();
-            } while (result != "Invalid username!");
+            } while (result != "** A valid Username ! **");
             do
             { Console.Write("Please enter the password: ");
                 string passWord1 = Console.ReadLine();
                 Console.Write("Please re-enter your password to confirm: ");
                 string passWord2 = Console.ReadLine();
+                Console.WriteLine(ValidatePassword(passWord1, passWord2));
 
                 result = ValidatePassword(passWord1, passWord2);
                 Console.WriteLine();
 
-            } while (result != "Password accepted !" || result != " ");//check
+            } while (result != "** Password accepted ! **");//check
+
+
+            //Displaying a message for setting username and password
+            Console.WriteLine("Congratulations! Your username and password have been accepted and set.");
             Console.ReadLine();
 
             
 
         }
+        /*Validate usename method checks the length of username and returns a result 
+        informing the user whether their username meet the requirements or not.*/
         static string ValidateUsername(string userName)
         {
             string result;
-            if (userName.Length == 8)
-            { result = "A valid Username"; }
+            if (userName.Length >= 8)
+            { result = "** A valid Username ! **"; }
             else
-            {  result = "Invalid username!";}
+            {  result = "Invalid username! It must include atleast 8 characters.";}
             return result;
         }
 
+        /*Validate password method checks the length of password and also checks if both the passwords are
+        same and if all the criteria is correct a message is sent of passwords acceptance otherwise an error message.*/
         static string ValidatePassword(string passWord1, string passWord2)
         { string result2;
-            if (passWord1.Length >= 8)//look at
+            if (passWord1.Length >= 8 )//look at
             {
                 if (passWord1 == passWord2)
-                { result2 = "Password accepted !"; }
+                { result2 = "** Password accepted ! **"; }
                 else
                 { result2 = "Error! your passwords don't match"; }
             }
+            
+   
             else
             { result2 = "Oops! Your password must include 8 characters"; }
             return result2;
