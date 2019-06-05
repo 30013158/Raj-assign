b72f9ea39d7137d855a8349e6ab4d90feec7de41
Rajveer Kaur <30013158@student.toiohomai.ac.nz>
2019-06-05 13:22:58 +1200

    first

--- Ques3/Ques3/Program.cs
+++ Ques3/Ques3/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Name : Rajveer Kaur
+//Task3 - Adding 2 matrices together
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,29 +11,72 @@ namespace Ques3
     class Program
     {
         static void Main(string[] args)
-        {
+        {  
+            //Random numbers
             Random rand = new Random();
-            double [,] matrixA = new double[3, 3];
+
+           
+            //initialising Matrix A's array using random numbers and using nested for loop
+            int[,] matrixA = new int[3, 3];
             
-            for (int i = 0; i <= 3; i++)
+            for (int i = 0; i < matrixA.GetLength(0); i++)
             {
-                for(int j=0; j <= 3; j++)
+                for(int j=0; j < matrixA.GetLength(1); j++)
                 { matrixA[i, j] = rand.Next(-10, 10); }
-                Console.WriteLine();
             }
+            //displaying Matrix A
             Console.WriteLine("Matrix A: ");
             Display(matrixA);
+
+            Console.WriteLine();
+
+
+
+            ////initialising Matrix A's array using random numbers and using nested for loop
+            int[,] matrixB = new int[3, 3];
+
+            for (int i = 0; i < matrixB.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixB.GetLength(1); j++)
+                { matrixB[i, j] = rand.Next(-10, 10); }
+
+            }
+            //displaying Matrix B
+            Console.WriteLine("Matrix B: ");
+            Display(matrixB);
+
+            Console.WriteLine();
+
+
+
+            //matrix C = matrix A = matrix B
+
+            int[,] matrixC = new int[3, 3];
+            for (int i = 0; i < matrixC.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixC.GetLength(1); j++)
+                { matrixC[i, j] = matrixA[i, j] + matrixB[i, j];  }
+            }
+            Console.WriteLine("Matrix C = Matrix A + Matrix B ");
+            Display(matrixC);
+
+
+
             Console.ReadLine();
             
         }
-        static void Display(double[,] arr)
+       
+        //a method for displaying the arrays
+        static void Display(int[,] arr)
         {
             for (int i = 0; i < arr.GetLength(0); i++)
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                     Console.Write(arr[i, j] + " ");
+
+                Console.WriteLine();
             }
-            Console.WriteLine();
+
         }
     }
 }
